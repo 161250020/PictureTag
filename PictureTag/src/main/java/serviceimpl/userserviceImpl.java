7cb8813244d8185fcaea0bc31811254ebc5c4036
161250029 <161250029@smail.nju.edu.cn>
2018-04-29 16:01:47 +0800

    //类加载器

--- PictureTag/src/main/java/serviceimpl/userserviceImpl.java
+++ PictureTag/src/main/java/serviceimpl/userserviceImpl.java
@@ -13,7 +13,8 @@ import java.util.ArrayList;
 public class userserviceImpl implements user{
             //final String path="PictureTag/src/main/user.txt";
             //URL pathURL = Thread.currentThread().getContextClassLoader().getResource("");
-            final String path = "user.txt";
+            //final String path = "user.txt";
+            final String path=UserInfo.class.getResource("/").getFile()+File.separator+"user.data";
             public void start() {      //一开始有一个用户
                 UserInfo user=new UserInfo("admin","admin",null,0,null,null,0,0.0);
                 Gson gson=new Gson();
