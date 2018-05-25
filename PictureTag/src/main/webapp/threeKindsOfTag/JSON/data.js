070fc51a55b40f3402735a15be67f3890e777964
161250020 <799544882@qq.com>
2018-05-25 19:36:44 +0800

    重写项目的三种标注

---
+++ PictureTag/src/main/webapp/threeKindsOfTag/JSON/data.js
@@ -0,0 +1,24 @@
+{	
+    "show":[
+        {
+            "title":"Cavaliers",
+            "des":"Cleveland",
+            "imgpath":"img/2.jpg"
+        },
+        {"title":"Bulls","des":"Chicago","imgpath":"img/1.jpg"},
+        {"title":"Warriors","des":"Golden State","imgpath":"img/3.jpg"},
+        {"title":"Heat","des":"Miami","imgpath":"img/4.jpg"},
+        {"title":"Thunder","des":"Oklahoma des","imgpath":"img/5.jpg"},
+        {"title":"Spurs","des":"San Antonio","imgpath":"img/6.jpg"},
+        {"title":"Rockets","des":"Houston","imgpath":"img/7.jpg"},
+        {"title":"Lakers","des":"Los Angeles","imgpath":"img/8.jpg"},
+        {"title":"Bucks","des":"Milwaukee","imgpath":"img/9.jpg"},
+        {"title":"Celtics","des":"Boston","imgpath":"img/10.jpg"},
+        {"title":"Hornets","des":"Charlotte","imgpath":"img/5.jpg"},
+        {"title":"Pistons","des":"Detroit","imgpath":"img/4.jpg"},
+        {"title":"Mavericks","des":"Dallas","imgpath":"img/3.jpg"},
+        {"title":"Hawks","des":"Atlanta","imgpath":"img/2.jpg"},
+        {"title":"Grizzlies","des":"Memphis","imgpath":"img/1.jpg"},
+        {"title":"Raptors","des":"Toronto","imgpath":"img/8.jpg"}
+        ]
+}
