978c415b29ed908a17a88d8508bfe7629dd5b8b3
wcc <941574987@qq.com>
2018-06-16 17:59:09 +0800

    change

--- PictureTag/src/main/java/serviceimpl/tagAccuracy/analyzeTagAccuracyImpl.java
+++ PictureTag/src/main/java/serviceimpl/tagAccuracy/analyzeTagAccuracyImpl.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class analyzeTagAccuracyImpl implements analyzeTagAccuracyService {
 
     serviceimpl.tag.imageServiceImpl imageServiceImpl = new imageServiceImpl();
-    String checkTaskFileName = analyzeTagAccuracyImpl.class.getResource("/").getFile()+ File.separator+"checkTask.task";
+    public String checkTaskFileName = analyzeTagAccuracyImpl.class.getResource("/").getFile()+ File.separator+"checkTask.task";
 
     public void checkTagAccuracy(boolean b){
         if(true == b){
