2ea5ce1d64436383f8385e7f75f4230cd450f43e
161250029 <161250029@smail.nju.edu.cn>
2018-06-17 12:14:36 +0800

    //年度统计

--- PictureTag/src/main/java/serviceimpl/task/AdminUser.java
+++ PictureTag/src/main/java/serviceimpl/task/AdminUser.java
@@ -53,8 +53,30 @@ public class AdminUser {
               }
               return count;
           }
+
+          public ArrayList<String> getUserCountByYear(String year){
+              ArrayList<String> result=new ArrayList<String>();
+              ArrayList<UserInfo> all=getAll();
+              ArrayList<UserInfo> newlist=new ArrayList<UserInfo>();
+              for(UserInfo user:all){
+                  if(user.getDate().substring(0,4).equals(year)){
+                      newlist.add(user);
+                  }
+              }
+              int counts[]={0,0,0,0,0,0,0,0,0,0,0,0};
+              for(UserInfo user:newlist){
+                   counts[getMonthIndex(user.getMonth())]++;
+              }
+              for(int i=0;i<counts.length;i++){
+                  String temp=""+counts[i];
+                  result.add(temp);
+              }
+              return result;
+          }
+
+
           //辅助方法
-          public int checkMonth(String month){
+          public int checkMonth(String month){            //返回该月的天数
               int day=0;
               if(month.equals("01")||month.equals("03")||month.equals("05")||month.equals("07")||month.equals("08")||month.equals("10")||month.equals("12")){
                   day=31;
@@ -68,7 +90,7 @@ public class AdminUser {
               return day;
           }
 
-          public int convertDay(String day){
+          public int convertDay(String day){                         //将day转为整数
               int result=0;
               if(day.substring(0,1).equals("0")){
                   result=Integer.parseInt(day.substring(1,2));
@@ -77,5 +99,46 @@ public class AdminUser {
                   result=Integer.parseInt(day);
               }
               return result;
+          }
+
+          public int getMonthIndex(String month){
+              int index=0;
+              if(month.equals("01")){
+                  index=0;
+              }
+              else if(month.equals("02")){
+                  index=1;
+              }
+              else if(month.equals("03")){
+                  index=2;
+              }
+              else if(month.equals("04")){
+                  index=3;
+              }
+              else if(month.equals("05")){
+                  index=4;
+              }
+              else if(month.equals("06")){
+                  index=5;
+              }
+              else if(month.equals("07")){
+                  index=6;
+              }
+              else if(month.equals("08")){
+                  index=7;
+              }
+              else if(month.equals("09")){
+                  index=8;
+              }
+              else if(month.equals("10")){
+                  index=9;
+              }
+              else if(month.equals("11")){
+                  index=10;
+              }
+              else{
+                  index=11;
+              }
+              return index;
           }
 }
