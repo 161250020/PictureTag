b57b348ea3c6ba177c4c12ab5324cc0dad40326f
161250029 <161250029@smail.nju.edu.cn>
2018-06-17 09:04:24 +0800

    //删去管理员列表

--- PictureTag/src/main/java/serviceimpl/task/AdminUser.java
+++ PictureTag/src/main/java/serviceimpl/task/AdminUser.java
@@ -7,7 +7,14 @@ import java.util.ArrayList;
 
 public class AdminUser {
           userserviceImpl impl=new userserviceImpl();
-          public ArrayList<UserInfo> getAll(){
-               return impl.getall();
+          public ArrayList<UserInfo> getAll() {
+              ArrayList<UserInfo> list = impl.getall();
+              ArrayList<UserInfo> newList = new ArrayList<UserInfo>();
+              for (UserInfo user : list) {
+                  if (!user.getUsername().equals("admin")) {
+                      newList.add(user);
+                  }
+              }
+              return newList;
           }
 }
