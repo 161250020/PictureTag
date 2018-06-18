1523be525bdb7f2ab2bdf60acbdf3d8deb575ce1
161250029 <161250029@smail.nju.edu.cn>
2018-06-18 15:43:41 +0800

    //账单更新

--- PictureTag/src/main/java/serviceimpl/Bill/BillServiceImpl.java
+++ PictureTag/src/main/java/serviceimpl/Bill/BillServiceImpl.java
@@ -12,20 +12,30 @@ public class BillServiceImpl {
       private userserviceImpl impl=new userserviceImpl();
       private taskServiceImpl service=new taskServiceImpl();
       private Gson gson=new Gson();
-      public ArrayList<Task> BillTasks(String username){
-            ArrayList<Task> tasks=new ArrayList<Task>();
-            UserInfo user=impl.getUser(username);
-            for(String str:user.getReceiveEvalu().keySet()){
-                tasks.add(gson.fromJson(service.receiveTaskInfo(str),Task.class));
-            }
-            return tasks;
-      }
       public ArrayList<String> BillCounts(String username){
           ArrayList<String> result=new ArrayList<String>();
-          ArrayList<Task> tasks=BillTasks(username);
+          ArrayList<Task> tasks=hh(username);
           for(Task task:tasks){
               result.add(""+(task.getSocre()*task.getGrade()/100.0));
           }
           return result;
       }
-}
+      public ArrayList<String> BillTasks(String username){
+          ArrayList<String> taskIds=new ArrayList<String>();
+          UserInfo user=impl.getUser(username);
+          for(String str:user.getReceiveEvalu().keySet()){
+              taskIds.add(str);
+          }
+          return taskIds;
+      }
+
+      //辅助方法
+      public ArrayList<Task> hh(String username){
+        ArrayList<Task> tasks=new ArrayList<Task>();
+        UserInfo user=impl.getUser(username);
+        for(String str:user.getReceiveEvalu().keySet()){
+            tasks.add(gson.fromJson(service.receiveTaskInfo(str),Task.class));
+         }
+        return tasks;
+      }
+   }
